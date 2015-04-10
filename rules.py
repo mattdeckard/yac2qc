3499842391896d3bcd4cab2db32bc29e76618e31
Matt Deckard <matthew.d.deckard@gmail.com>
2015-04-09 21:17:05 -0500

    Comment out all rules

--- rules.py
+++ rules.py
@@ -49,28 +49,28 @@ _rule = _namedtuple('mapping_rule', 'category code namedesc '
 # just some examples with fake names and numbers.
 # These should be modified for your own needs. Comments can be deleted.
 rules = [
-    # rule that matches when the transaction is from a 'Betaalautomaat (BA)'
-    # and has the name 'special fuels incorporated'
-    _rule('car - fuel', 'BA', 'special fuels incorporated', None, None),
-    # similar for 'CleanFuelInc'
-    _rule('car - fuel', 'BA', 'CleanFuelInc', None, None),
-    # with my bank ST means 'deposit'
-    _rule('cash - deposit', 'ST', None, None, None),
-    # .. and 'GM' means 'withdrawal' at a cash machine
-    _rule('cash - withdrawal', 'GM', None, None, None),
-    # just some fake supermarkets
-    _rule('supermarket - galbert geijn', 'BA', 'Galbert Geijn', None, None),
-    _rule('supermarket - mega', 'BA', 'MEGA', None, None),
-    _rule('supermarket - smallmart', 'BA', 'Small Mart', None, None),
-    # and other types of stores
-    _rule('store - Hamma', 'BA', 'Hamma', None, None),
-    _rule('store - Gema', 'BA', 'GEMA', None, None),
-    _rule('store - AEKI', 'BA', 'Inter AEKI Systems', None, None),
-    _rule('store - warkei', 'BA', 'WarKei Bouwmarkt', None, None),
-    # specific accounts can also be defined as follows
-    _rule('account - savings', None, None, '0123456789', None),
-    # when the account number matches and some number appears in the
-    # 'description' field, this is a statement related to income tax
-    _rule('taxes - income tax', None, None, '999999999999', '1111.1111.111'),
+    ## rule that matches when the transaction is from a 'Betaalautomaat (BA)'
+    ## and has the name 'special fuels incorporated'
+    #_rule('car - fuel', 'BA', 'special fuels incorporated', None, None),
+    ## similar for 'CleanFuelInc'
+    #_rule('car - fuel', 'BA', 'CleanFuelInc', None, None),
+    ## with my bank ST means 'deposit'
+    #_rule('cash - deposit', 'ST', None, None, None),
+    ## .. and 'GM' means 'withdrawal' at a cash machine
+    #_rule('cash - withdrawal', 'GM', None, None, None),
+    ## just some fake supermarkets
+    #_rule('supermarket - galbert geijn', 'BA', 'Galbert Geijn', None, None),
+    #_rule('supermarket - mega', 'BA', 'MEGA', None, None),
+    #_rule('supermarket - smallmart', 'BA', 'Small Mart', None, None),
+    ## and other types of stores
+    #_rule('store - Hamma', 'BA', 'Hamma', None, None),
+    #_rule('store - Gema', 'BA', 'GEMA', None, None),
+    #_rule('store - AEKI', 'BA', 'Inter AEKI Systems', None, None),
+    #_rule('store - warkei', 'BA', 'WarKei Bouwmarkt', None, None),
+    ## specific accounts can also be defined as follows
+    #_rule('account - savings', None, None, '0123456789', None),
+    ## when the account number matches and some number appears in the
+    ## 'description' field, this is a statement related to income tax
+    #_rule('taxes - income tax', None, None, '999999999999', '1111.1111.111'),
 ]
 
